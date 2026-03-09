db6b013dae8c70e1ec8f19227839c06f1afcd609
MatteoCiardi <ciardimatteo99@gmail.com>
2026-03-09 16:14:01 +0100

    milestone 3 completata

--- src/main.ts
+++ src/main.ts
@@ -25,3 +25,39 @@ type Actress = Person & {
   awards: string
   nationality: ActressNationality,
 }
+
+function isActress(dati: unknown): dati is Actress {
+  return (
+    typeof dati === 'object' && dati !== null &&
+    "id" in dati && typeof dati.id === 'number' &&
+    "name" in dati && typeof dati.name === 'string' &&
+    "birth_year" in dati && typeof dati.birth_year === 'number' &&
+    "death_year" in dati && typeof dati.birth_year === 'number' &&
+    "biography" in dati && typeof dati.biography === 'string' &&
+    "image" in dati && typeof dati.image === 'string' &&
+    "most_famous_movies" in dati &&
+    dati.most_famous_movies instanceof Array &&
+    dati.most_famous_movies.length === 3 &&
+    dati.most_famous_movies.every(m => typeof m === 'string') &&
+    "awards" in dati && dati.awards === 'string' &&
+    "nationality" in dati && typeof dati.nationality === 'string'
+  )
+}
+
+async function getActress(id: number) {
+  try {
+    const response = await fetch(`http://localhost:3333/actresses/${id}`);
+    const data: unknown = await response.json();
+    if (!isActress(data)) {
+      throw new Error("Dati non validi")
+    }
+    return data;
+  } catch (error) {
+    if(error instanceof Error) {
+      console.error('Errore durante il recupero dei dati:', error)
+    } else {
+      console.error('Errore sconosciuto:', error)
+    }
+    return null
+  } 
+}
